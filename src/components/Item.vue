c450ef95123abcfeb86aef4138835ec2dcabc662
kamesh.ss <kameshoddanchatram@gmail.com>
2019-02-26 23:51:33 +0530

    item component added

---
+++ src/components/Item.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="item" @click="this.click(id)">
+        {{title}}
+    </div>
+</template>
+
+<script>
+export default {
+  name : "Item",
+  props: {
+    id            : Number,
+    title         : String,
+    description   : String
+  }
+};
+</script>
+
+<style  scoped lang="scss">
+  .item {
+    color         : #444;
+    background    : #ededed;
+    border-radius : 5px;
+    font-size     : 14px;
+    padding       : 5px;
+    margin        : 4px;
+    text-align    : left;
+  }
+</style>
