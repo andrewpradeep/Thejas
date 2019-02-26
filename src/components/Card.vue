44c1ddff86577da9a0d1eb189b91497e68d2d896
pradeep-zt201 <jawaharpradeep001@gmail.com>
2019-02-27 00:37:08 +0530

    card component added

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <span @click="openOptions">...</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+    data(){
+      return {
+          cardtitle : this.title
+      }
+    },
+  props: ["title"],
+  methods: {
+    openOptions: function() {
+      alert("options have been loaded");
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
